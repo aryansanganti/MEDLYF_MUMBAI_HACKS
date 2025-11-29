d07e97f2e1c0de0aecaf03157e72499fb5233edf
Aryan Sanganti <aryansanganti@gmail.com>
2025-11-29 08:54:22 +0530

    hello

--- server/models/Hospital.js
+++ server/models/Hospital.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose');
 
 // Allow schema-less read of existing docs
-const hospitalSchema = new mongoose.Schema({}, { strict: false });
+const hospitalSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    city: { type: String, required: true },
+    icuBeds: { type: Number, required: true },
+    occupancy: { type: Number, required: true }, // Percentage 0-100
+    oxygenLevel: { type: Number, required: true } // Percentage 0-100
+});
 
 module.exports = mongoose.model('Hospital', hospitalSchema, 'Hospitals');
